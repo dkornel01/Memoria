f6a9a778be9c9bd49023ed4027c6f27d69fd276d
dkornel01 <115635103+dkornel01@users.noreply.github.com>
2023-05-17 09:34:43 +0200

    Add files via upload

---
+++ babu.js
@@ -0,0 +1,29 @@
+class Babu{
+    constructor(){
+    //this.szin=szin
+    //x_hely=
+    //y_hely=
+    //id_hely=
+    }
+    getSzin(){
+
+    }
+    setId_hely(){
+
+    }
+    getId_hely(){
+
+    }
+    gety_hely(){
+
+    }
+    gety_hely(){
+
+    }
+    getx_hely(){
+
+    }
+    getx_hely(){
+
+    }
+}
